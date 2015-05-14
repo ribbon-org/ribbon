e731af23edb2fa0d7dbeecbd51bbd7ceee1afb8c
takudo <taku.doyao@gmail.com>
2015-05-14 14:32:00 +0900

    first commit

---
+++ src/main/java/org/ribbon/Index.java
@@ -0,0 +1,21 @@
+package org.ribbon;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Index {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Index.class, args);
+    }
+
+}
